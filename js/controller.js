143816bd92edcf21b03a1ae4a3b445f3c080f9d8
emerkel <eddie.merkel@neeralu.com>
2014-02-04 15:39:36 -0600

    I should have done this earlier but here it is working pretty much like I want
    The only thing I need yet is some verbiage and to get the tabs to work
    the way you would think they would.

---
+++ js/controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* Controllers */
+
+var neeraluSolutions = angular.module('neeraluSolutions', []);
+
+neeraluSolutions.controller('neeraluController', function($scope) {
+  $scope.phoneNumber = {'405.598.7323'};
+});
--- js/neeralu.js
+++ js/neeralu.js
@@ -1,11 +1,35 @@
 
+
 /*
 These methods just show the appropriate div and selects the tab when the user clicks the button--just as if they had clicked the tab instead.
 */
-$('#gotoDataManagement').on('click', function() {
-    $('#myTab a[href="#dropdown1"]').tab('show')
-});
+//$('#gotoDataManagement').on('click', function() {
+//    $('#myTab a[href="#dropdown1"]').tab('show')
+//});
 
-$('#gotoCloudMigration').on('click', function() {
-    $('#myTab a[href="#dropdown2"]').tab('show')
-});
+//$('#gotoCloudMigration').on('click', function() {
+//    $('#myTab a[href="#dropdown2"]').tab('show')
+//});
+
+function getFirebaseData(){
+    var dataRef = new Firebase('https://neeralusite.firebaseio.com/PhoneNumber');
+    dataRef.once('value', function(snapshot) {
+      var phoneNum = snapshot.val();
+      //alert('phone number is ' + phoneNum);
+      displayPhoneNumber(phoneNum);
+    });
+}
+
+function displayPhoneNumber(phoneNumber ){
+    document.getElementById('phoneNumberSpan').innerHTML = 'phone: ' + phoneNumber;
+    document.getElementById( 'phoneNumberSpan2').innerHTML = 'phone: ' + phoneNumber;
+}
+
+function showTab(tabName){
+	switch(tabName){
+		case 'dataManagement':
+			$('#myTab a[href="#dropdown1"]').tab('show')
+			break;
+		default:
+	}
+}
