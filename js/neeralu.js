19a348bf046b746b87448570d78aed10f447ccc8
emerkel <eddie.merkel@neeralu.com>
2014-02-06 15:52:02 -0600

    simplified everything
    took out all the firebase calls and just keep the phone number in a
    global variable now. minimizes the problems and simplifies a simple
    problem from what was a complex solution that didn't work correctly
    anyway.

--- js/neeralu.js
+++ js/neeralu.js
@@ -1,57 +1,63 @@
-var neeraluPhoneNumber = '405-359-9640';
+var neeraluPhoneNumber = "&#52;&#48;&#53;&#45;&#53;&#57;&#56;&#45;&#55;&#51;&#50;&#51;";
 
+//var getTodaysDate = function(){
+//	
+//	var q = new Date();
+//	var m = q.getMonth();
+//	var d = q.getDate();
+//	var y = q.getYear();
+//	
+//	return new Date(y, m, d);
+//}
 
-var getFirebaseData = function (){
-	// if we have not already gotten the data for today, we will get it and store it
-	// in the local storage
-	var q = new Date();
-	var m = q.getMonth() + 1;
-	var d = q.getDay();
-	var y = q.getYear();
-	var goFetch = false;
-	
-	if (typeof(Storage) !== "undefined")
-	{
-		var today = new Date(y, m, d);
-		var firebaseDate = localStorage.getItem('firebaseDate');
-		var fbPhoneNumber = localStorage.getItem('fbPhoneNumber');
-		
-		if (firebaseDate != null)
-		{
-			if (firebaseDate != today)
-			{
-				// get the phone 
-				goFetch = true;
-			}
-		} else {
-			// get the phone number
-			goFetch = true;
-		}
-		
-		if (goFetch)
-		{
-		    var dataRef = new Firebase('https://neeralusite.firebaseio.com/PhoneNumber');
-		    dataRef.once('value', function(snapshot) {
-		      var phoneNum = snapshot.val();
-		      //alert('phone number is ' + phoneNum);
-		      fbPhoneNumber = phoneNum;
-		      localStorage.setItem('fbPhoneNumber', fbPhoneNumber);
-		      localStorage.setItem('firebaseDate', today);
-		    });
-		}
-	}
-	else
-	{
-		fbPhoneNumber = neeraluPhoneNumber;
-	}
-	
-	return fbPhoneNumber;
-}
+//var getFirebaseData = function (){
+//	// if we have not already gotten the data for today, we will get it and store it
+//	// in the local storage
+//	
+//	var goFetch = false;
+//	
+//	if (typeof(Storage) !== "undefined")
+//	{
+//		var today = getTodaysDate();
+//		var firebaseDate = localStorage.getItem('firebaseDate');
+//		var fbPhoneNumber = localStorage.getItem('fbPhoneNumber');
+//		
+//		if (firebaseDate != null)
+//		{
+//			if (firebaseDate != today)
+//			{
+//				// get the data 
+//				goFetch = true;
+//			}
+//		} else {
+//			// get the data
+//			goFetch = true;
+//		}
+//		
+//		if (goFetch)
+//		{
+//		    var dataRef = new Firebase('https://neeralusite.firebaseio.com/PhoneNumber');
+//		    dataRef.once('value', function(snapshot) {
+//		      var phoneNum = snapshot.val();
+//
+//		      fbPhoneNumber = phoneNum;
+//		      localStorage.setItem('fbPhoneNumber', fbPhoneNumber);
+//		      localStorage.setItem('firebaseDate', today);
+//		    });
+//		}
+//	}
+//	else
+//	{
+//		fbPhoneNumber = defaultPhoneNumber;
+//	}
+//	
+//	return fbPhoneNumber;
+//}
 
 var showTab = function(tabName){
 	
 	switch(tabName){
-		case 'dataManagement':
+		case "dataManagement":
 			jQuery('#myTab a[href="#dropdown1"]').tab('show')
 			break;
 		default:
